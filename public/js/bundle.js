7bd4992c284203def6a5cdc7c0f14243d527c08c
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2020-11-14 12:46:36 +0000

    test

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6245,24 +6245,249 @@ module.exports = setup;
 /*!*********************!*\
   !*** ./src/main.ts ***!
   \*********************/
-/*! flagged exports */
-/*! export __esModule [provided] [no usage info] [missing usage info prevents renaming] */
-/*! other exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_exports__, __webpack_require__ */
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements: top-level-this-exports, __webpack_require__ */
+/*! CommonJS bailout: this is used directly at 2:17-21 */
+/*! CommonJS bailout: this is used directly at 11:19-23 */
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-const io = __webpack_require__(/*! socket.io-client */ "./node_modules/socket.io-client/build/index.js");
-const socket = io.connect();
-socket.emit('connection');
-const video = document.querySelector('#videoPlayer');
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(stream => {
-    video.srcObject = stream;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var socketIO = __webpack_require__(/*! socket.io-client */ "./node_modules/socket.io-client/build/index.js");
+var socket = socketIO.connect();
+var video = document.querySelector('#videoPlayer');
+var connect = document.querySelector('#connect');
+var textToReceiveSdp = document.querySelector('#receiveSdp');
+var resiveRemoteButton = document.querySelector('#receiveRemoteSdp');
+var textForSendSdp = document.querySelector('#sendSdp');
+var localStream = null;
+var peerConnection = null;
+var negotiationneededCounter = 0;
+var isOffer = false;
+var prepareNewConnection = function (isOffer) {
+    // const pc_config = {"iceServers":[ {"urls":"stun:stun.webrtc.ecl.ntt.com:3478"} ]};
+    var peer = new RTCPeerConnection();
+    peer.ontrack = function (evt) {
+        console.log('-- peer.ontrack()');
+        playVideo(video, evt.streams[0]);
+    };
+    peer.onicecandidate = function (evt) {
+        if (evt.candidate) {
+            console.log(evt.candidate);
+        }
+        else {
+            console.log('empty ice event');
+            sendSdp(peer.localDescription);
+        }
+    };
+    peer.onnegotiationneeded = function () { return __awaiter(void 0, void 0, void 0, function () {
+        var offer, err_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 4, , 5]);
+                    if (!isOffer) return [3 /*break*/, 3];
+                    return [4 /*yield*/, peer.createOffer()];
+                case 1:
+                    offer = _a.sent();
+                    console.log('createOffer() success in promise');
+                    return [4 /*yield*/, peer.setLocalDescription(offer)];
+                case 2:
+                    _a.sent();
+                    console.log('setLocalDescription() success in promise');
+                    sendSdp(peer.localDescription);
+                    negotiationneededCounter++;
+                    _a.label = 3;
+                case 3: return [3 /*break*/, 5];
+                case 4:
+                    err_1 = _a.sent();
+                    console.error("setLocalDescription(offer) ERROR: " + err_1);
+                    return [3 /*break*/, 5];
+                case 5: return [2 /*return*/];
+            }
+        });
+    }); };
+    if (localStream) {
+        console.log('Adding local stream...');
+        localStream.getTracks().forEach(function (track) { return peer.addTrack(track, localStream); });
+    }
+    else {
+        console.warn('no local stream, but continue.');
+    }
+    console.log(peer);
+    return peer;
+};
+var playVideo = function (element, stream) {
+    element.srcObject = stream;
+    element.play()
+        .catch(function (err) { return console.error(err); });
+};
+var sendSdp = function (sessionDescription) {
+    console.log('---send sdp---');
+    textForSendSdp.value = sessionDescription.sdp;
+};
+var makeAnswer = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var answer, err_2;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                console.log('sending Answer. Creating remote session description...');
+                if (!peerConnection) {
+                    console.error('peerConnection NOT exist!');
+                    return [2 /*return*/];
+                }
+                _a.label = 1;
+            case 1:
+                _a.trys.push([1, 4, , 5]);
+                return [4 /*yield*/, peerConnection.createAnswer()];
+            case 2:
+                answer = _a.sent();
+                console.log('createAnswer() success in promise');
+                return [4 /*yield*/, peerConnection.setLocalDescription(answer)];
+            case 3:
+                _a.sent();
+                console.log('setLocalDescription() success in promise');
+                sendSdp(peerConnection.localDescription);
+                return [3 /*break*/, 5];
+            case 4:
+                err_2 = _a.sent();
+                console.error(err_2);
+                return [3 /*break*/, 5];
+            case 5: return [2 /*return*/];
+        }
+    });
+}); };
+resiveRemoteButton.addEventListener('click', function () {
+    var text = textToReceiveSdp.value.replace(/\n|\r\n|\r/g, '');
+    if (peerConnection) {
+        console.log('Received answer text...');
+        var answer = new RTCSessionDescription({
+            type: 'answer',
+            sdp: text
+        });
+        setAnswer(answer);
+    }
+    else {
+        console.log('Received offer text...');
+        var offer = new RTCSessionDescription({
+            type: 'offer',
+            sdp: text
+        });
+        setOffer(offer);
+    }
+    textToReceiveSdp.value = '';
+});
+var setOffer = function (sessionDescription) { return __awaiter(void 0, void 0, void 0, function () {
+    var err_3;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (peerConnection) {
+                    console.error('peerConnection already exist!');
+                }
+                peerConnection = prepareNewConnection(false);
+                _a.label = 1;
+            case 1:
+                _a.trys.push([1, 3, , 4]);
+                return [4 /*yield*/, peerConnection.setRemoteDescription(sessionDescription)];
+            case 2:
+                _a.sent();
+                console.log('setRemoteDescription(answer) success in promise');
+                makeAnswer();
+                return [3 /*break*/, 4];
+            case 3:
+                err_3 = _a.sent();
+                console.error("setRemoteDescription(offer) ERROR: " + err_3);
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); };
+var setAnswer = function (sessionDescription) { return __awaiter(void 0, void 0, void 0, function () {
+    var err_4;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!peerConnection) {
+                    console.error('peerConnection NOT exist!');
+                    return [2 /*return*/];
+                }
+                _a.label = 1;
+            case 1:
+                _a.trys.push([1, 3, , 4]);
+                return [4 /*yield*/, peerConnection.setRemoteDescription(sessionDescription)];
+            case 2:
+                _a.sent();
+                console.log("setRemoteDescription(answer) success in promise");
+                return [3 /*break*/, 4];
+            case 3:
+                err_4 = _a.sent();
+                console.error("setRemoteDescription(answer) ERROR: " + err_4);
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); };
+navigator.mediaDevices.getUserMedia({
+    video: {
+        width: {
+            exact: 640
+        },
+        height: {
+            exact: 480
+        }
+    },
+    audio: false
 })
-    .catch(err => console.error(err));
+    .then(function (stream) {
+    playVideo(video, stream);
+})
+    .catch(function (err) { return console.error(err); });
+connect.addEventListener('click', function () {
+    if (!peerConnection) {
+        console.log('make offer');
+        peerConnection = prepareNewConnection(true);
+    }
+    else {
+        console.warn('peer already exist.');
+    }
+});
 
 
 /***/ }),
@@ -6367,7 +6592,7 @@ module.exports = yeast;
 /******/ 		};
 /******/ 	
 /******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -6376,8 +6601,8 @@ module.exports = yeast;
 /************************************************************************/
 /******/ 	// startup
 /******/ 	// Load entry module
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
 /******/ 	__webpack_require__("./src/main.ts");
-/******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
